d698139d8a8d64e23cc99bdd877da2f631b48fda
Hung Nguyen <honghung.dotdak@gmail.com>
2022-04-23 16:49:40 +0700

    feat: grpc health check

---
+++ pgrpc/healthcheck.go
@@ -0,0 +1,19 @@
+package pgrpc
+
+import (
+	"context"
+
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+func NewGrpcHealthCheckServer() grpc_health_v1.HealthServer {
+	return &GrpcHealthCheckServerImpl{}
+}
+
+type GrpcHealthCheckServerImpl struct {
+	grpc_health_v1.UnimplementedHealthServer
+}
+
+func (s *GrpcHealthCheckServerImpl) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	return &grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_SERVING}, nil
+}
